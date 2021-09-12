2fc17cddbd6aaa5d15c7e08fb9b2245fbb8d234a
ugurkellecioglu <ugur.kellecioglu@outlook.com>
2021-09-12 07:48:28 +0300

    user selected cssed

--- src/Components/UserSelected/UserSelected.jsx
+++ src/Components/UserSelected/UserSelected.jsx
@@ -3,7 +3,13 @@ import "../../Styles/css/UserSelected.css"
 
 function UserSelected() {
     return (
-        <div>
+        <div className="UserSelectedWrapper">
+            <div className="Card">
+                <div className="CardBody">
+                    <div className="Title">Welcome</div>
+                    <div className="Info">Has your situation changed? Update your profile 💪</div>
+                </div> 
+            </div>
         </div>
     )
 }
