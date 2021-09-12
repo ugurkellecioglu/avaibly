9931bbefe2c45efed44091793c11e10d776c0515
ugurkellecioglu <ugur.kellecioglu@outlook.com>
2021-09-12 07:35:36 +0300

    status selected and userselected css

--- src/Components/StatusSelected/StatusSelected.jsx
+++ src/Components/StatusSelected/StatusSelected.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "../../Styles/css/StatusSelected.css"
 function StatusSelected() {
     return (
         <div>
--- src/Components/UserSelected/UserSelected.jsx
+++ src/Components/UserSelected/UserSelected.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../../Styles/css/UserSelected.css"
 
 function UserSelected() {
     return (
